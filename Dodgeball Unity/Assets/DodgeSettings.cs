733c6c0eb4e5b0fe78818c1fa9c609c34f241f52
Evan Coulter <evan@Evans-MacBook-Pro.local>
2021-12-04 11:47:40 -0800

    Copied ML Agents Example Project, Moved Dodgeball Scripts to Assets, & Added Sample Scene.

---
+++ Dodgeball Unity/Assets/DodgeSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class DodgeSettings : MonoBehaviour
+{
+    public Material purpleMaterial;
+    public Material blueMaterial;
+    public bool randomizePlayersTeamForTraining = true;
+    public float agentRunSpeed;
+}
